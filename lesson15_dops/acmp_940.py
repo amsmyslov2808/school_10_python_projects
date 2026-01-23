fd14fe56f88cabd32ab35a7740fb8294f3d3511a
amsmyslov <a.m.smyslov@yandex.ru>
2026-01-23 21:10:41 +0300

    add lesson 15 dops

---
+++ lesson15_dops/acmp_940.py
@@ -0,0 +1,16 @@
+file_in = open("INPUT.txt", "r", encoding="utf-8")
+file_out = open("OUTPUT.txt", "w", encoding="utf-8")
+
+data = file_in.readline()
+
+first_space = data.find(" ")
+
+k = int(data[0:first_space]) - 1
+s = data[first_space:].strip()
+
+result = s[:k] + s[k + 1 :]
+
+file_out.write(result)
+
+file_in.close()
+file_out.close()
