2cb2c7dfb56237de4c4fc679865f456a4afef721
amsmyslov <a.m.smyslov@yandex.ru>
2026-03-08 19:58:31 +0300

    add some func

--- lesson22_tamagochi/Tamagotchi.py
+++ lesson22_tamagochi/Tamagotchi.py
@@ -12,6 +12,9 @@ class Tamagotchi:
     __MIN_LEVEL = 0
     __MAX_LEVEL = 10
 
+    __MIN_INCREASE = 1
+    __MAX_ICREASE = 3
+
     def __init__(self, name: str, type_animal: str, age: int):
         self.__name = name
         self.__type_animal = type_animal
@@ -21,3 +24,20 @@ class Tamagotchi:
             self.__MIN_LEVEL + 5, self.__MAX_LEVEL - 3
         )
         self.__energy_level = random.randint(self.__MIN_LEVEL + 5, self.__MAX_LEVEL - 3)
+
+    def is_alive(self) -> bool:
+        return self.__health_level > self.__MIN_LEVEL
+
+    def feed(self) -> None:
+        increase = random.randint(self.__MIN_INCREASE, self.__MAX_ICREASE)
+
+        self.__energy_level = self.__energy_level + increase
+
+        if self.__energy_level > self.__MAX_LEVEL:
+            self.__energy_level = self.__MAX_LEVEL
+
+            print(
+                f"{self.__name} переел. его уровень энергии достиг максимума. его здоровье ухудшается на 1 пункт"
+            )
+
+            self.__health_level = self.__health_level - 1
