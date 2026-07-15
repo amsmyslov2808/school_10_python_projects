746f0fbac8f31064d1fcae7d483e103bf072c69b
amsmyslov <a.m.smyslov@yandex.ru>
2026-07-15 19:54:04 +0300

    222

--- lesson41_sql_alchemy/steam_proc_crud_sql_al.py
+++ lesson41_sql_alchemy/steam_proc_crud_sql_al.py
@@ -1,7 +1,19 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped
+)
 
 DATABASE_URL = "postgresql+psycopg://postgres:12345@localhost:5432/steam_db"
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class UserRole(Base):
+    __tablename__ = "user_roles"
+    
+    id: Mapped[int] = 
+    role_name: Mapped[str] = 
+    description: Mapped[str] =
+    
