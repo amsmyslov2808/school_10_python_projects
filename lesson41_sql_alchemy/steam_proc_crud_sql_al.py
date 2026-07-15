83d26a3af90bda37e6d2a7c8199f541f7370cbac
amsmyslov <a.m.smyslov@yandex.ru>
2026-07-15 19:58:59 +0300

    333

--- lesson41_sql_alchemy/steam_proc_crud_sql_al.py
+++ lesson41_sql_alchemy/steam_proc_crud_sql_al.py
@@ -1,6 +1,22 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Boolean,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    create_engine,
+    select,
+)
 from sqlalchemy.orm import (
     DeclarativeBase,
-    Mapped
+    Mapped,
+    Session,
+    joinedload,
+    mapped_column,
+    relationship,
+    sessionmaker,
 )
 
 DATABASE_URL = "postgresql+psycopg://postgres:12345@localhost:5432/steam_db"
@@ -12,8 +28,7 @@ class Base(DeclarativeBase):
 
 class UserRole(Base):
     __tablename__ = "user_roles"
-    
-    id: Mapped[int] = 
-    role_name: Mapped[str] = 
-    description: Mapped[str] =
-    
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    role_name: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
