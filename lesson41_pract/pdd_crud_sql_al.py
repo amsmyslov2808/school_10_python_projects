e6e1003cc1a36cdb8f48331f0731bc78c8732483
amsmyslov <a.m.smyslov@yandex.ru>
2026-07-17 20:00:29 +0300

    add SignCategory

--- lesson41_pract/pdd_crud_sql_al.py
+++ lesson41_pract/pdd_crud_sql_al.py
@@ -22,3 +22,10 @@ DATABASE_URL = "postgresql+psycopg://postgres:12345@localhost:5432/pdd_db"
 
 class Base(DeclarativeBase):
     pass
+
+
+class SignCategory(Base):
+    __tablename__ = "sign_categories"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
