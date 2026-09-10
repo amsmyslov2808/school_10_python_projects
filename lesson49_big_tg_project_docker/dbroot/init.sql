e52ecfaff11d58e57b7f60247c7becdc8d0006ad
amsmyslov <a.m.smyslov@yandex.ru>
2026-09-10 14:28:20 +0300

    add comments for 49 lesson

--- lesson49_big_tg_project_docker/dbroot/init.sql
+++ lesson49_big_tg_project_docker/dbroot/init.sql
@@ -1,5 +1,7 @@
+-- Таблица создаётся автоматически при первой инициализации контейнера PostgreSQL.
 CREATE TABLE IF NOT EXISTS visited_cities
 (
+    -- Уникальный идентификатор записи генерируется базой данных.
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     tg_user_id bigint NOT NULL,
     name character varying(50) NOT NULL,
